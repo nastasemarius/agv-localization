07fe2d2c6ebbdb275e234ab3eb5b22386b10f39d
Nastase Marius <nastase.marius94@gmail.com>
2019-03-11 17:07:14 +0200

    estetic improvements for map and toolbar

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+
 declare var H: any;
+const PIXEL_RATIO = window.devicePixelRatio;
 
 @Component({
   selector: "app-map",
@@ -8,6 +10,10 @@ declare var H: any;
 })
 export class MapComponent implements OnInit {
   private platform: any;
+  private defaultLayers: any;
+  private map: any;
+  private behavior: any;
+  private ui: any;
 
   @ViewChild("map")
   public mapElement: ElementRef;
@@ -17,19 +23,30 @@ export class MapComponent implements OnInit {
       app_id: "13hwYirltXKWTWuVQBHT",
       app_code: "81n03Tl0Oobk_csyXiyPLQ"
     });
+
+    this.defaultLayers = this.platform.createDefaultLayers({
+      tileSize: PIXEL_RATIO === 1 ? 256 : 512,
+      ppi: PIXEL_RATIO === 1 ? undefined : 320
+    });
   }
 
   public ngOnInit() {}
 
   public ngAfterViewInit() {
-    let defaultLayers = this.platform.createDefaultLayers();
-    let map = new H.Map(
+    this.map = new H.Map(
       this.mapElement.nativeElement,
-      defaultLayers.normal.map,
+      this.defaultLayers.normal.map,
       {
         zoom: 10,
-        center: { lat: 37.7397, lng: -121.4252 }
+        center: { lat: 37.7397, lng: -121.4252 },
+        pixelRatio: PIXEL_RATIO
       }
     );
+
+    this.behavior = new H.mapevents.Behavior(
+      new H.mapevents.MapEvents(this.map)
+    );
+
+    this.ui = H.ui.UI.createDefault(this.map, this.defaultLayers);
   }
 }
